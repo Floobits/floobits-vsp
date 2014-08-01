647152d8236d1752746928850b2b5de8b36675e6
Rob Emanuele <rob@emanuele.us>
2014-08-01 02:17:48 -0700

    Initial Import

---
+++ floobits-vsp/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace Floobits.floobits_vsp
+{
+    static class PkgCmdIDList
+    {
+        public const uint cmdidJoinWorkspace =        0x200;
+        public const uint cmdidFlooTool =    0x101;
+
+    };
+}
